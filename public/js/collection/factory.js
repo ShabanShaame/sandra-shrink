18ee61df989ed572359e03566f4832970f5cba69
Millord <t.roffat@everdreamsoft.com>
2020-10-20 14:57:35 +0200

    modif call ajax for simplify

--- public/js/collection/factory.js
+++ public/js/collection/factory.js
@@ -2,7 +2,7 @@ jQuery(document).ready(function($){
 
     if(refMap != 'null' && table != 'null'){
 
-        console.log(refMap);
+        $('.spinner-grow').show()
 
         const references = refMap.replace('[', '').replace(']', '');
         const refColumns = references.split(',');
@@ -17,42 +17,14 @@ jQuery(document).ready(function($){
             }
         });
 
-        $.ajax({
-            url: '/api/collection/'+db+'/'+env+'/get/'+table,
-            type: 'get',
-            beforeSend:function(){
-                $('.spinner-grow').show();
-            },
-            success: function(response){
-                $('.spinner-grow').hide()
-                ajaxServerSide()
-                changeView()
-            },
-            error: function (jqXHR, exception){
-
-                $msg = '';
-
-                if (jqXHR.status === 0) {
-                    msg = 'Not connect.\n Verify Network.';
-                } else if (jqXHR.status == 404) {
-                    msg = 'Requested page not found. [404]';
-                } else if (jqXHR.status == 500) {
-                    msg = 'Internal Server Error [500].';
-                } else if (exception === 'timeout') {
-                    msg = 'Time out error.';
-                } else if (exception === 'abort') {
-                    msg = 'Ajax request aborted.';
-                } else {
-                    msg = 'Uncaught Error.\n' + jqXHR.responseText;
-                }
-                $('#jsonAlert').slideDown(500).html($msg);
-            }
-        })
-
+        ajaxServerSide()
+        changeView()
 
         function ajaxServerSide(){
 
             $table = $('#factoryTable');
+
+            $('.spinner-grow').hide()
 
             $table.DataTable({
                 processing: true,
@@ -85,8 +57,6 @@ jQuery(document).ready(function($){
 
 
     }
-
-
 
 
 })
